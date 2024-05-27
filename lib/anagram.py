ac6bfe8b50178de04f401d2a1a0d26a78a8c7abc
Gustavo <alex.kimani@student.moringaschool.com>
2024-05-27 09:09:30 +0300

    Done with assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+    
+    def match(self,word_list):
+        matches = []
+
+        for potential_anagram in word_list:
+            if sorted(potential_anagram) == sorted(self.word) and potential_anagram!=self.word:
+                matches.append(potential_anagram)
+        return matches
